3bdaf9087cfaa9ac69d0ed2d06ca5954bbe3826e
Jemin Desai <jemindesai@yahoo.com>
2025-09-27 17:46:35 -0500

    Making sidebar into a toggle

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-document.getElementById("clickMe").addEventListener("click", () => {
-  alert("You clicked the button!");
+const sidebar = document.getElementById("sidebar");
+const content = document.querySelector(".content");
+const toggleBtn = document.getElementById("toggleBtn");
+
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("closed");
+  content.classList.toggle("expanded");
 });
+
